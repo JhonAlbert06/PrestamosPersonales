8f94ad622cee715b4948750d49fcb1b0279dd628
JhonAlbert06 <jhonalbertguzmanrosario@gmail.com>
2022-09-01 16:32:41 -0400

    Diseño Simple de Registro

--- app/src/main/java/edu/ucne/prestamospersonales/MainActivity.kt
+++ app/src/main/java/edu/ucne/prestamospersonales/MainActivity.kt
@@ -3,13 +3,23 @@ package edu.ucne.prestamospersonales
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.materialIcon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import edu.ucne.prestamospersonales.ui.theme.PrestamosPersonalesTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +32,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android studio")
+                    CuadroCompose(modifier = Modifier.fillMaxSize())
                 }
             }
         }
@@ -30,14 +40,68 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun CuadroCompose(modifier: Modifier)
+{
+    val focusManager = LocalFocusManager.current
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(18.dp)
+    ) {
+
+        Text(text = "Prestamos Personales")
+
+        CuadroTexto(
+            descripcion = "Descripción",
+            KeyboardOptions(
+                keyboardType = KeyboardType.Text,
+                imeAction = ImeAction.Done
+            ),
+            keyboardActions = KeyboardActions(onNext = {focusManager.clearFocus()})
+        )
+
+        CuadroTexto(
+            descripcion = "Salario",
+            KeyboardOptions(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Done
+            ),
+            keyboardActions = KeyboardActions(onDone = {focusManager.clearFocus()}))
+
+        OutlinedButton(
+            onClick = { },
+            modifier = Modifier.fillMaxWidth().padding(18.dp),
+            shape = CircleShape,
+            border= BorderStroke(1.dp, Color.Blue)
+
+        ) {
+            Icon(Icons.Default.Add, contentDescription = "content description")
+            Text(text = "Guardar")
+        }
+    }
+}
+
+@Composable
+fun CuadroTexto(
+    descripcion : String,
+    keyboardOptions: KeyboardOptions,
+    keyboardActions: KeyboardActions
+){
+    OutlinedTextField(
+        modifier = Modifier.fillMaxWidth(),
+        label = { Text(text = descripcion) },
+        value = "", 
+        onValueChange = {},
+        keyboardOptions = keyboardOptions,
+        keyboardActions = keyboardActions,
+        singleLine = true)
 }
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun DefaultPreview() {
     PrestamosPersonalesTheme {
-        Greeting("Android studio")
+        CuadroCompose(modifier = Modifier.fillMaxSize())
     }
 }
