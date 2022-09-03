9ac176da07d73f1d117374a1372500133ee262bd
JhonAlbert06 <jhonalbertguzmanrosario@gmail.com>
2022-09-02 21:48:31 -0400

    Eliminacion de import

--- app/src/main/java/edu/ucne/prestamospersonales/di/AppModule.kt
+++ app/src/main/java/edu/ucne/prestamospersonales/di/AppModule.kt
@@ -2,7 +2,6 @@ package edu.ucne.prestamospersonales.di
 
 import android.app.Application
 import androidx.room.Room
-import androidx.room.RoomDatabase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
