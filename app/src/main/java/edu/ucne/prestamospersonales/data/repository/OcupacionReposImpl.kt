930d7ca28f77930e31f83185619ac92c615b9c91
JhonAlbert06 <jhonalbertguzmanrosario@gmail.com>
2022-09-02 21:35:34 -0400

    Implementacion del ocupaciones repositorio y el dao

--- app/src/main/java/edu/ucne/prestamospersonales/data/repository/OcupacionReposImpl.kt
+++ app/src/main/java/edu/ucne/prestamospersonales/data/repository/OcupacionReposImpl.kt
@@ -1,7 +1,29 @@
 package edu.ucne.prestamospersonales.data.repository
 
+import edu.ucne.prestamospersonales.data.local.dao.OcupacionDao
+import edu.ucne.prestamospersonales.domain.model.Ocupacion
+import edu.ucne.prestamospersonales.domain.repository.OcupacionRepository
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
-class OcupacionReposImpl @Inject constructor(){
+class OcupacionReposImpl @Inject constructor(
+    private val dao: OcupacionDao
+): OcupacionRepository{
+
+    override suspend fun inserOcupacion(Ocupacion: Ocupacion) {
+        return dao.inserOcupacion(Ocupacion)
+    }
+
+    override suspend fun deleteOcupacion(Ocupacion: Ocupacion) {
+        return dao.deleteOcupacion(Ocupacion)
+    }
+
+    override fun getOcupaciones(): Flow<List<Ocupacion>> {
+        return getOcupaciones()
+    }
+
+    override suspend fun getOcupacionesById(id: Int): Ocupacion? {
+        return getOcupacionesById(id)
+    }
 
 }
