f81264f3c03a2dd87598ba95d7332ae80d5b0525
JhonAlbert06 <jhonalbertguzmanrosario@gmail.com>
2022-09-02 21:47:18 -0400

    Proveer Repository y base de datos

---
+++ app/src/main/java/edu/ucne/prestamospersonales/di/AppModule.kt
@@ -0,0 +1,33 @@
+package edu.ucne.prestamospersonales.di
+
+import android.app.Application
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import edu.ucne.prestamospersonales.data.local.OcupacionDB
+import edu.ucne.prestamospersonales.data.repository.OcupacionReposImpl
+import edu.ucne.prestamospersonales.domain.repository.OcupacionRepository
+import edu.ucne.prestamospersonales.utils.DATABASE
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun providePrestamosPersonales(app: Application) = Room.databaseBuilder(
+        app,
+        OcupacionDB::class.java,
+        DATABASE
+    ).build()
+
+    @Provides
+    @Singleton
+    fun provideRepository(db: OcupacionDB): OcupacionRepository{
+        return OcupacionReposImpl(db.ocupacionDao)
+    }
+}
---
+++ app/src/main/java/edu/ucne/prestamospersonales/utils/Constans.kt
@@ -0,0 +1,3 @@
+package edu.ucne.prestamospersonales.utils
+
+const val DATABASE = "PrestamosP_db"
