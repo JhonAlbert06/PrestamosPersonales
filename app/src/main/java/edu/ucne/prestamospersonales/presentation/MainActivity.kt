a2b14110a257f82a2279b79a14975971b1713818
JhonAlbert06 <jhonalbertguzmanrosario@gmail.com>
2022-09-08 19:25:14 -0400

    todo

--- app/src/main/java/edu/ucne/prestamospersonales/presentation/MainActivity.kt
+++ app/src/main/java/edu/ucne/prestamospersonales/presentation/MainActivity.kt
@@ -3,7 +3,10 @@ package edu.ucne.prestamospersonales.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 import dagger.hilt.android.AndroidEntryPoint
+import edu.ucne.prestamospersonales.presentation.edit.Validacion
 import edu.ucne.prestamospersonales.ui.theme.PrestamosPersonalesTheme
 
 @AndroidEntryPoint
@@ -15,5 +18,13 @@ class MainActivity : ComponentActivity() {
                 Navigation()
             }
         }
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun PrevieeScreen(){
+    PrestamosPersonalesTheme {
+        Navigation()
     }
 }
