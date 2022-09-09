de29585f10f26d5c68842ccae9babbe79ba5bf84
JhonAlbert06 <jhonalbertguzmanrosario@gmail.com>
2022-09-08 20:39:01 -0400

    Instancias Sin Injeccion

--- app/src/main/java/edu/ucne/prestamospersonales/data/AppDataBase.kt
+++ app/src/main/java/edu/ucne/prestamospersonales/data/AppDataBase.kt
@@ -1,6 +1,8 @@
 package edu.ucne.prestamospersonales.data
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import edu.ucne.prestamospersonales.data.dao.OcupacionDao
 import edu.ucne.prestamospersonales.data.model.Ocupacion
@@ -12,4 +14,28 @@ import edu.ucne.prestamospersonales.data.model.Ocupacion
 
 abstract class AppDataBase : RoomDatabase() {
     abstract val ocupacionDao: OcupacionDao
+
+    companion object {
+
+        @Volatile
+        private var INSTANCE: AppDataBase? = null
+
+        fun getInstance(context: Context): AppDataBase {
+            synchronized(this) {
+                var instance = INSTANCE
+
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        AppDataBase::class.java,
+                        "AppDataBase"
+                    )
+                        .fallbackToDestructiveMigration()
+                        .build()
+                    INSTANCE = instance
+                }
+                return instance
+            }
+        }
+    }
 }
