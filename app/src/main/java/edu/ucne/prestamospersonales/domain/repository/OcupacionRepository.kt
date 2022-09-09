bd72fb598cf36cacebae3140294487dd52584013
JhonAlbert06 <jhonalbertguzmanrosario@gmail.com>
2022-09-09 09:57:11 -0400

    Creacion OcupacionRepository

---
+++ app/src/main/java/edu/ucne/prestamospersonales/domain/repository/OcupacionRepository.kt
@@ -0,0 +1,11 @@
+package edu.ucne.prestamospersonales.domain.repository
+
+import edu.ucne.prestamospersonales.data.model.Ocupacion
+import kotlinx.coroutines.flow.Flow
+
+interface OcupacionRepository {
+    suspend fun insert(ocupacion: Ocupacion)
+    suspend fun delete(ocupacion: Ocupacion)
+    suspend fun getById(id:Int): Ocupacion
+    fun getAll(): Flow<List<Ocupacion>>
+}
