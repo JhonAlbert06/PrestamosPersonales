060951d8b75064632655618f828a9b8974c59fb3
JhonAlbert06 <jhonalbertguzmanrosario@gmail.com>
2022-09-09 09:56:35 -0400

    Eliminando lo por defecto

--- app/src/main/java/edu/ucne/prestamospersonales/MainActivity.kt
+++ app/src/main/java/edu/ucne/prestamospersonales/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import edu.ucne.prestamospersonales.data.AppDataBase
 import edu.ucne.prestamospersonales.ui.theme.PrestamosPersonalesTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +18,22 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             PrestamosPersonalesTheme {
-                // A surface container using the 'background' color from the theme
+
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun DefaultPreview() {
     PrestamosPersonalesTheme {
-        Greeting("Android")
+
     }
 }
