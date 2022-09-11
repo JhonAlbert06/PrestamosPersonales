bca8cd8e658d48b7daee12426e7378e052353194
JhonAlbert06 <jhonalbertguzmanrosario@gmail.com>
2022-09-11 12:36:27 -0400

    Main

--- app/src/main/java/edu/ucne/prestamospersonales/MainActivity.kt
+++ app/src/main/java/edu/ucne/prestamospersonales/MainActivity.kt
@@ -3,13 +3,28 @@ package edu.ucne.prestamospersonales
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import edu.ucne.prestamospersonales.feature_personas.presentation.components.ElijeFecha
+import edu.ucne.prestamospersonales.feature_personas.presentation.components.InputText
+import edu.ucne.prestamospersonales.feature_personas.presentation.components.TextBox
+import edu.ucne.prestamospersonales.feature_personas.presentation.edit.*
 import edu.ucne.prestamospersonales.ui.theme.PrestamosPersonalesTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,22 +37,19 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+
+
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun DefaultPreview() {
     PrestamosPersonalesTheme {
-        Greeting("Android")
+
     }
 }
