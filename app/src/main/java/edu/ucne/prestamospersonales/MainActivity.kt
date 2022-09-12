7eb16f552358821883e9703ac975fdaac5500a98
JhonAlbert06 <jhonalbertguzmanrosario@gmail.com>
2022-09-12 18:45:28 -0400

    Main

--- app/src/main/java/edu/ucne/prestamospersonales/MainActivity.kt
+++ app/src/main/java/edu/ucne/prestamospersonales/MainActivity.kt
@@ -3,36 +3,16 @@ package edu.ucne.prestamospersonales
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import edu.ucne.prestamospersonales.ui.theme.PrestamosPersonalesTheme
-
+import dagger.hilt.android.AndroidEntryPoint
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             PrestamosPersonalesTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Navigation()
-                }
+                Navigation()
             }
         }
-    }
-}
-
-
-@Preview(showBackground = true, showSystemUi = true)
-@Composable
-fun DefaultPreview() {
-    PrestamosPersonalesTheme {
-
     }
 }
