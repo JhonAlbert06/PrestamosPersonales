23e2774cc2e8a55577585c5a14a0ee21028ff896
JhonAlbert06 <jhonalbertguzmanrosario@gmail.com>
2022-09-12 21:23:58 -0400

    Model Persona

--- app/src/main/java/edu/ucne/prestamospersonales/data/model/Persona.kt
+++ app/src/main/java/edu/ucne/prestamospersonales/data/model/Persona.kt
@@ -6,12 +6,12 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "Personas")
 data class Persona(
     @PrimaryKey(autoGenerate = true)
-    val personaId: Int? = null,
+    val personaId: Int? = 0,
     val nombres: String = "",
     val telefono: Long = 0,
     val celular: Long = 0,
     val email: String = "",
     val direccion: String = "",
     val fechaNacimiento: String = "",
-    val ocupacionId: Int? = null,
+    val ocupacionId: Int? = 0,
 )
