82504ac55332f82409824e895ed86252f2eb891b
JhonAlbert06 <jhonalbertguzmanrosario@gmail.com>
2022-09-20 16:30:25 -0400

    DataBase

---
+++ app/src/main/java/edu/ucne/prestamospersonales/data/AppDataBase.kt
@@ -0,0 +1,24 @@
+package edu.ucne.prestamospersonales.data
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import edu.ucne.prestamospersonales.data.dao.OcupacionDao
+import edu.ucne.prestamospersonales.data.dao.PersonaDao
+import edu.ucne.prestamospersonales.data.dao.PrestamoDao
+import edu.ucne.prestamospersonales.data.models.Ocupacion
+import edu.ucne.prestamospersonales.data.models.Persona
+import edu.ucne.prestamospersonales.data.models.Prestamo
+
+@Database(
+    entities = [
+        Ocupacion::class,
+        Persona::class,
+        Prestamo::class],
+    version = 1
+)
+
+abstract class AppDataBase: RoomDatabase() {
+    abstract val ocupacionDao: OcupacionDao
+    abstract val personaDao: PersonaDao
+    abstract val prestamoDao: PrestamoDao
+}
