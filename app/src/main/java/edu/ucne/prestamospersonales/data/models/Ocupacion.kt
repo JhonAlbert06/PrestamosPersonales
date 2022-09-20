cd5cbba8e618ae72e183d1497ab83a7260b2a8c3
JhonAlbert06 <jhonalbertguzmanrosario@gmail.com>
2022-09-20 16:29:53 -0400

    Models

---
+++ app/src/main/java/edu/ucne/prestamospersonales/data/models/Ocupacion.kt
@@ -0,0 +1,12 @@
+package edu.ucne.prestamospersonales.data.models
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "Ocupaciones")
+data class Ocupacion(
+    @PrimaryKey(autoGenerate = true)
+    val ocupacionId: Int? = null,
+    val descripcion: String = "",
+    val salario: Double = 0.0
+)
