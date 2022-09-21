c6abe96cb33e661e415b90f48bb0e05cfd1f8802
JhonAlbert06 <jhonalbertguzmanrosario@gmail.com>
2022-09-20 21:45:22 -0400

    Screen

--- app/src/main/java/edu/ucne/prestamospersonales/Screen.kt
+++ app/src/main/java/edu/ucne/prestamospersonales/Screen.kt
@@ -1,9 +1,14 @@
 package edu.ucne.prestamospersonales
 
 sealed class Screen(val route: String){
-    object EditOcupacion: Screen("editOcupacion?ocupacionId={ocupacionId}"){
+    /*object EditOcupacion: Screen("editOcupacion?ocupacionId={ocupacionId}"){
         fun passId(ocupacionId: Int?): String {
             return "editOcupacion?ocupacionId=$ocupacionId"
+        }
+    }*/
+    object EditPersona: Screen("editPersona?personaId={personaId}"){
+        fun passId(personaId: Int?): String {
+            return "editPersona?personaId=$personaId"
         }
     }
 }
