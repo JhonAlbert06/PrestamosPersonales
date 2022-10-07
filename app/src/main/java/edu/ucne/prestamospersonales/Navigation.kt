ef3aa08375cd3de4186bea4b7f1f25e39af84c88
JhonAlbert06 <jhonalbertguzmanrosario@gmail.com>
2022-10-06 20:55:17 -0400

    Navigation

--- app/src/main/java/edu/ucne/prestamospersonales/Navigation.kt
+++ app/src/main/java/edu/ucne/prestamospersonales/Navigation.kt
@@ -6,22 +6,19 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
-import edu.ucne.prestamospersonales.presentation.consulta.homePrestamo.PrestamosList
-import edu.ucne.prestamospersonales.presentation.registros.editPrestamo.PrestamoScreen
-
 
 @Composable
 fun Navigation() {
     val navController = rememberNavController()
     NavHost(
         navController = navController,
-        startDestination = Screen.HomePrestamo.route
+        startDestination = "Screen.HomePrestamo.route"
     ){
-        composable(route = Screen.HomePrestamo.route){
-            PrestamosList(navController = navController)
+        composable(route = "Screen.HomePrestamo.route"){
+            //PrestamosScreen(navController = navController)
         }
         composable(
-            route = Screen.EditPrestamo.route,
+            route = "Screen.EditPrestamo.route",
             arguments = listOf(
                 navArgument(
                     name = "ocupacionId"
@@ -31,7 +28,7 @@ fun Navigation() {
                 }
             )
         ){
-            PrestamoScreen(navController = navController)
+            //PrestamoScreen(navController = navController)
         }
     }
 }
--- app/src/main/java/edu/ucne/prestamospersonales/Screen.kt
+++ app/src/main/java/edu/ucne/prestamospersonales/Screen.kt
@@ -1,20 +1,25 @@
 package edu.ucne.prestamospersonales
 
 sealed class Screen(val route: String){
-    /*object EditOcupacion: Screen("editOcupacion?ocupacionId={ocupacionId}"){
+    /*
+    object EditOcupacion: Screen("editOcupacion?ocupacionId={ocupacionId}"){
         fun passId(ocupacionId: Int?): String {
             return "editOcupacion?ocupacionId=$ocupacionId"
         }
-    }*/
-    /*object EditPersona: Screen("editPersona?personaId={personaId}"){
+    }
+
+    object EditPersona: Screen("editPersona?personaId={personaId}"){
         fun passId(personaId: Int?): String {
             return "editPersona?personaId=$personaId"
         }
-    }*/
+    }
+
     object HomePrestamo: Screen("homePersona")
+
     object EditPrestamo: Screen("editPrestamo?prestamoId={prestamoId}"){
         fun passId(prestamoId: Int?): String {
             return "editPrestamo?prestamoId=$prestamoId"
         }
     }
+    */
 }
