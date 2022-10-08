07d3b286af57aec577780d64233ba6e90138beef
JhonAlbert06 <jhonalbertguzmanrosario@gmail.com>
2022-10-07 22:29:15 -0400

    InputText Update

--- app/src/main/java/edu/ucne/prestamospersonales/ui/components/InputText.kt
+++ app/src/main/java/edu/ucne/prestamospersonales/ui/components/InputText.kt
@@ -25,7 +25,7 @@ fun InputText(
     Column(
         modifier = modifier
             .fillMaxWidth()
-            .padding(start = 16.dp, end = 16.dp, bottom = 16.dp)
+            .padding(start = 16.dp, end = 16.dp, bottom = 8.dp)
             .padding(
                 bottom = if (isError){
                     0.dp
