7172b5e7f80aa4c102815c76b37f9285a4411cb5
JhonAlbert06 <jhonalbertguzmanrosario@gmail.com>
2022-10-08 19:01:36 -0400

    PrestamoEditScreen Update

--- app/src/main/java/edu/ucne/prestamospersonales/ui/prestamo/PrestamoEditScreen.kt
+++ app/src/main/java/edu/ucne/prestamospersonales/ui/prestamo/PrestamoEditScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusDirection
@@ -19,19 +20,32 @@ import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.core.text.isDigitsOnly
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import edu.ucne.prestamospersonales.R
 import edu.ucne.prestamospersonales.ui.components.ComboBoxPersona
 import edu.ucne.prestamospersonales.ui.components.ComboFecha
 import edu.ucne.prestamospersonales.ui.components.InputText
-import edu.ucne.prestamospersonales.ui.persona.PersonaEditEvent
+import edu.ucne.prestamospersonales.ui.persona.isNumeric
+import kotlinx.coroutines.flow.collectLatest
 
 @Composable
 fun PrestamoEditScreen(
     navController: NavController,
     viewModel: PrestamoEditViewModel = hiltViewModel()
 ) {
+
+    LaunchedEffect(key1 = true) {
+        viewModel.eventFlow.collectLatest { event ->
+            when (event) {
+                is PrestamoEditViewModel.UiEvent.SavePrestamo -> {
+                    navController.navigateUp()
+                }
+            }
+        }
+    }
+
 
     Scaffold(
 
@@ -85,6 +99,18 @@ fun PrestamoEditBottomBar(
 @Composable
 fun PrestamoEditConten(
     viewModel: PrestamoEditViewModel,
+    isErrorFechaPrestamo:Boolean,
+    isErrorFechaVence:Boolean,
+    isErrorPersonaId:Boolean,
+    isErrorConcepto:Boolean,
+    isErrorMonto:Boolean,
+    isErrorBalance:Boolean,
+    mgsFechaPrestamo:String,
+    mgsFechaVence:String,
+    mgsPersonaId:String,
+    mgsConcepto:String,
+    mgsMonto:String,
+    mgsBalance:String,
     onEvent: (PrestamoEditEvent) -> Unit
 ) {
     Column(
@@ -94,16 +120,16 @@ fun PrestamoEditConten(
 
         Spacer(modifier = Modifier.height(44.dp))
 
-        viewModel.fechaPrestamo.value.text = ComboFecha()
-        viewModel.fechaVence.value.text = ComboFecha()
-        viewModel.personaId.value.text = ComboBoxPersona().toString()
+        viewModel.fechaPrestamo.value.text = ComboFecha(isErrorFechaPrestamo, mgsFechaPrestamo)
+        viewModel.fechaVence.value.text = ComboFecha(isErrorFechaVence, mgsFechaVence)
+        viewModel.personaId.value.text = ComboBoxPersona(isErrorPersonaId, mgsPersonaId).toString()
 
         InputText(
-            isError = true,
-            msgError = "*Campo Obligatorio*",
+            isError = isErrorConcepto,
+            msgError = mgsConcepto,
             text = viewModel.concepto.value.text,
             label = stringResource(id = R.string.Concepto),
-            onTextChange = {onEvent(PrestamoEditEvent.EnteredConcepto(it)) },
+            onTextChange = { onEvent(PrestamoEditEvent.EnteredConcepto(it)) },
             keyboardOptions = KeyboardOptions(
                 keyboardType = KeyboardType.Text,
                 imeAction = ImeAction.Next
@@ -114,8 +140,8 @@ fun PrestamoEditConten(
         )
 
         InputText(
-            isError = true,
-            msgError = "*Campo Obligatorio*",
+            isError = isErrorMonto,
+            msgError = mgsMonto,
             text = viewModel.monto.value.text,
             label = stringResource(id = R.string.Monto),
             onTextChange = { onEvent(PrestamoEditEvent.EnteredMonto(it)) },
@@ -129,8 +155,8 @@ fun PrestamoEditConten(
         )
 
         InputText(
-            isError = true,
-            msgError = "*Campo Obligatorio*",
+            isError = isErrorBalance,
+            msgError = mgsBalance,
             text = viewModel.balance.value.text,
             label = stringResource(id = R.string.Balance),
             onTextChange = { onEvent(PrestamoEditEvent.EnteredBalance(it)) },
@@ -168,6 +194,81 @@ fun validacion(
     viewModel: PrestamoEditViewModel,
     onEvent: (PrestamoEditEvent) -> Unit
 ): Boolean {
-    PrestamoEditConten(viewModel = viewModel, onEvent = onEvent)
+
+    var isErrorFechaPrestamo = false
+    var isErrorFechaVence = false
+    var isErrorPersonaId = false
+    var isErrorConcepto = false
+    var isErrorMonto = false
+    var isErrorBalance = false
+
+    var mgsFechaPrestamo = ""
+    var mgsFechaVence = ""
+    var mgsPersonaId = ""
+    var mgsConcepto = ""
+    var mgsMonto = ""
+    var mgsBalance = ""
+
+    if (viewModel.fechaPrestamo.value.text.isBlank()) {
+        isErrorFechaPrestamo = true
+        mgsFechaPrestamo = "*Campo Obligatorio*"
+    }
+
+    if (viewModel.fechaVence.value.text.isBlank()) {
+        isErrorFechaVence = true
+        mgsFechaVence = "*Campo Obligatorio*"
+    }
+
+    if (viewModel.personaId.value.text.isBlank()) {
+        isErrorPersonaId = true
+        mgsPersonaId = "*Campo Obligatorio*"
+    }
+
+    if (viewModel.concepto.value.text.isEmpty()) {
+        isErrorConcepto = true
+        mgsConcepto = "*Campo Obligatorio*"
+    } else if (viewModel.concepto.value.text.isDigitsOnly()) {
+        isErrorConcepto = true
+        mgsConcepto = "Solo se permiten Caracteres"
+    } else if (!(viewModel.concepto.value.text.any { it.isLetter() })) {
+        isErrorConcepto = true
+        mgsConcepto = "No se permiten simbolos"
+    }
+
+    if (viewModel.monto.value.text.isEmpty()) {
+        isErrorMonto = true
+        mgsMonto = "*Campo Obligatorio*"
+    } else if (!isNumeric(viewModel.monto.value.text)) {
+        isErrorMonto = true
+        mgsMonto = "Monto no valido"
+    }
+
+    if (viewModel.balance.value.text.isEmpty()) {
+        isErrorBalance = true
+        mgsBalance = "*Campo Obligatorio*"
+    } else if (!isNumeric(viewModel.balance.value.text)) {
+        isErrorBalance = true
+        mgsBalance = "Balance no valido"
+    }
+
+
+    PrestamoEditConten(
+        viewModel = viewModel,
+        isErrorFechaPrestamo = isErrorFechaPrestamo,
+        isErrorFechaVence = isErrorFechaVence,
+        isErrorPersonaId = isErrorPersonaId,
+        isErrorConcepto = isErrorConcepto,
+        isErrorMonto = isErrorMonto,
+        isErrorBalance = isErrorBalance,
+        mgsFechaPrestamo = mgsFechaPrestamo,
+        mgsFechaVence = mgsFechaVence,
+        mgsPersonaId = mgsPersonaId,
+        mgsConcepto = mgsConcepto,
+        mgsMonto = mgsMonto,
+        mgsBalance = mgsBalance,
+        onEvent = onEvent
+    )
+
+
     return (true)
 }
