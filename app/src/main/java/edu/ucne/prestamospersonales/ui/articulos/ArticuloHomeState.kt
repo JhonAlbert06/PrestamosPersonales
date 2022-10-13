c60906585c18d7507cbebd46af35247f891edb94
JhonAlbert06 <jhonalbertguzmanrosario@gmail.com>
2022-10-13 17:41:38 -0400

    ArticuloHomeState Update

--- app/src/main/java/edu/ucne/prestamospersonales/ui/articulos/ArticuloHomeState.kt
+++ app/src/main/java/edu/ucne/prestamospersonales/ui/articulos/ArticuloHomeState.kt
@@ -3,7 +3,7 @@ package edu.ucne.prestamospersonales.ui.articulos
 import edu.ucne.prestamospersonales.data.remote.dto.ArticulosResponseDto
 
 data class ArticuloHomeState(
-    val isLoading: Boolean = false,
+    val isLoading: Boolean = true,
     val articulos: List<ArticulosResponseDto> = emptyList(),
     val error: String = ""
 )
