7b1542369e90f0cb854c7ea687ea5bac329ea2ec
JhonAlbert06 <jhonalbertguzmanrosario@gmail.com>
2022-10-13 16:27:07 -0400

    ArticulosResponseDto Update

--- app/src/main/java/edu/ucne/prestamospersonales/data/remote/dto/ArticulosResponseDto.kt
+++ app/src/main/java/edu/ucne/prestamospersonales/data/remote/dto/ArticulosResponseDto.kt
@@ -1,6 +1,6 @@
 package edu.ucne.prestamospersonales.data.remote.dto
 
-import com.squareup.moshi.JsonClass
+//import com.squareup.moshi.JsonClass
 
 //@JsonClass(generateAdapter = true)
 data class ArticulosResponseDto(
