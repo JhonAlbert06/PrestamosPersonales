bc052f6274f8088155f7e2a2362e2ff58486ef74
JhonAlbert06 <jhonalbertguzmanrosario@gmail.com>
2022-10-13 18:13:23 -0400

    ArticuloHomeEvent Update

--- app/src/main/java/edu/ucne/prestamospersonales/ui/articulos/ArticuloHomeEvent.kt
+++ app/src/main/java/edu/ucne/prestamospersonales/ui/articulos/ArticuloHomeEvent.kt
@@ -3,5 +3,5 @@ package edu.ucne.prestamospersonales.ui.articulos
 import edu.ucne.prestamospersonales.data.remote.dto.ArticulosResponseDto
 
 sealed class ArticuloHomeEvent{
-    data class  DeleteArticulo(val articulo: ArticulosResponseDto) : ArticuloHomeEvent()
+    data class  DeleteArticulo(val id: String) : ArticuloHomeEvent()
 }
